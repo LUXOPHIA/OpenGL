062e6dd849b821a669d033ea8e4a9cfc7edf9670
luxidea <contact@luxidea.net>
2017-05-02 05:17:49 +0900

    begin

---
+++ Main.pas
@@ -0,0 +1,134 @@
+﻿unit Main;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls,
+  Winapi.OpenGL, Winapi.OpenGLext,
+  LUX.GPU.OpenGL.FMX, LUX.GPU.OpenGL.FMX.GLView;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+      GLView1: TGLView;
+      GLView2: TGLView; 
+      GLView3: TGLView;
+      GLView4: TGLView;
+    Timer1: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { private 宣言 }
+    _Angle :Single;
+  public
+    { public 宣言 }
+    ///// メソッド
+    procedure DrawModel;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation //############################################################### ■
+
+{$R *.fmx}
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TForm1.DrawModel;
+begin
+     glBegin( GL_TRIANGLES );
+
+       glColor3f( 1.0, 0.0, 0.0 );  glVertex3f( +1.0, -1.0, +1.0 ); //R
+       glColor3f( 0.0, 1.0, 0.0 );  glVertex3f( +1.0, +1.0, -1.0 ); //G
+       glColor3f( 0.0, 0.0, 1.0 );  glVertex3f( -1.0, +1.0, +1.0 ); //B
+
+       glColor3f( 1.0, 1.0, 1.0 );  glVertex3f( -1.0, -1.0, -1.0 ); //W
+       glColor3f( 1.0, 0.0, 0.0 );  glVertex3f( +1.0, -1.0, +1.0 ); //R
+       glColor3f( 0.0, 0.0, 1.0 );  glVertex3f( -1.0, +1.0, +1.0 ); //B
+
+       glColor3f( 1.0, 1.0, 1.0 );  glVertex3f( -1.0, -1.0, -1.0 ); //W
+       glColor3f( 0.0, 0.0, 1.0 );  glVertex3f( -1.0, +1.0, +1.0 ); //B
+       glColor3f( 0.0, 1.0, 0.0 );  glVertex3f( +1.0, +1.0, -1.0 ); //G
+
+       glColor3f( 1.0, 1.0, 1.0 );  glVertex3f( -1.0, -1.0, -1.0 ); //W
+       glColor3f( 0.0, 1.0, 0.0 );  glVertex3f( +1.0, +1.0, -1.0 ); //G
+       glColor3f( 1.0, 0.0, 0.0 );  glVertex3f( +1.0, -1.0, +1.0 ); //R
+
+     glEnd;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+     _Angle := 0;
+
+     GLView1.OnPaint := procedure
+     begin
+          glMatrixMode( GL_PROJECTION );
+            glLoadIdentity;
+            glOrtho( -3, +3, -2, +2, 0.1, 100 );
+          glMatrixMode( GL_MODELVIEW );
+            glLoadIdentity;
+            glTranslatef( 0, 0, -5 );
+            glRotatef( _Angle, 0, 1, 0 );
+            DrawModel;
+     end;
+
+     GLView2.OnPaint := procedure
+     begin
+          glMatrixMode( GL_PROJECTION );
+            glLoadIdentity;
+            glOrtho( -4, +4, -2, +2, 0.1, 100 );
+          glMatrixMode( GL_MODELVIEW );
+            glLoadIdentity;
+            glTranslatef( 0, 0, -5 );
+            glRotatef( _Angle, 0, 1, 0 );
+            DrawModel;
+     end;
+
+     GLView3.OnPaint := procedure
+     begin
+          glMatrixMode( GL_PROJECTION );
+            glLoadIdentity;
+            glOrtho( -3, +3, -3, +3, 0.1, 100 );
+          glMatrixMode( GL_MODELVIEW );
+            glLoadIdentity;
+            glTranslatef( 0, 0, -5 );
+            glRotatef( _Angle, 0, 1, 0 );
+            DrawModel;
+     end;
+
+     GLView4.OnPaint := procedure
+     begin
+          glMatrixMode( GL_PROJECTION );
+            glLoadIdentity;
+            glOrtho( -4, +4, -3, +3, 0.1, 100 );
+          glMatrixMode( GL_MODELVIEW );
+            glLoadIdentity;
+            glTranslatef( 0, 0, -5 );
+            glRotatef( _Angle, 0, 1, 0 );
+            DrawModel;
+     end;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+     _Angle := _Angle + 1;
+
+     GLView1.Repaint;
+     GLView2.Repaint;
+     GLView3.Repaint;
+     GLView4.Repaint;
+end;
+
+end. //######################################################################### ■
---
+++ OpenGL.dpr
@@ -0,0 +1,36 @@
+program OpenGL;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Main in 'Main.pas' {Form1},
+  LUX.D3.V4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.V4.pas',
+  LUX.D4.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.M4.pas',
+  LUX.D4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.pas',
+  LUX.D4.V4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.V4.pas',
+  LUX.D5 in '_LIBRARY\LUXOPHIA\LUX\LUX.D5.pas',
+  LUX.Lattice.T1 in '_LIBRARY\LUXOPHIA\LUX\LUX.Lattice.T1.pas',
+  LUX.Lattice.T2 in '_LIBRARY\LUXOPHIA\LUX\LUX.Lattice.T2.pas',
+  LUX.Lattice.T3 in '_LIBRARY\LUXOPHIA\LUX\LUX.Lattice.T3.pas',
+  LUX.M2 in '_LIBRARY\LUXOPHIA\LUX\LUX.M2.pas',
+  LUX.M3 in '_LIBRARY\LUXOPHIA\LUX\LUX.M3.pas',
+  LUX.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.M4.pas',
+  LUX in '_LIBRARY\LUXOPHIA\LUX\LUX.pas',
+  LUX.D1 in '_LIBRARY\LUXOPHIA\LUX\LUX.D1.pas',
+  LUX.D2.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2.M4.pas',
+  LUX.D2 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2.pas',
+  LUX.D2.V4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2.V4.pas',
+  LUX.D3.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.M4.pas',
+  LUX.D3 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.pas',
+  LUX.GPU.OpenGL.FMX.GLView in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\LUX.GPU.OpenGL.FMX.GLView.pas' {GLView: TFrame},
+  LUX.GPU.OpenGL.FMX in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\LUX.GPU.OpenGL.FMX.pas',
+  LUX.GPU.OpenGL.Geometry in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\LUX.GPU.OpenGL.Geometry.pas',
+  LUX.GPU.OpenGL.Material in '_LIBRARY\LUXOPHIA\LUX.GPU.OpenGL\LUX.GPU.OpenGL.Material.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
