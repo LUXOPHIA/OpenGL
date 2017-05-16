0bc9252e12988500e1be6d7e13c09920281c27db
luxidea <contact@luxidea.net>
2017-05-16 10:50:48 +0900

    GLSL 1.5 仕様のシェーダへ変更。

--- _DATA/ShaderF.glsl
+++ _DATA/ShaderF.glsl
@@ -1,6 +1,35 @@
-﻿#version 130
+﻿#version 150
+
+////////////////////////////////////////////////////////////////////////////////【定数】
+
+layout(std140) uniform TCamera
+{
+  layout(row_major) mat4 Proj;
+  layout(row_major) mat4 Move;
+}
+_Camera;
+
+layout(std140) uniform TShape
+{
+  layout(row_major) mat4 Move;
+}
+_Shape;
+
+////////////////////////////////////////////////////////////////////////////////【入出力】
+
+in TSendVF
+{
+  vec4 Col;
+}
+_Sender;
+
+out vec4 _FragColor;
+
+//------------------------------------------------------------------------------
+
 void main()
 {
-  gl_FragColor = gl_Color;
+  _FragColor = _Sender.Col;
 }
 
+//##############################################################################
--- _DATA/ShaderV.glsl
+++ _DATA/ShaderV.glsl
@@ -1,7 +1,43 @@
-﻿#version 130
+﻿#version 150
+
+////////////////////////////////////////////////////////////////////////////////【定数】
+
+layout(std140) uniform TCamera
+{
+  layout(row_major) mat4 Proj;
+  layout(row_major) mat4 Move;
+}
+_Camera;
+
+layout(std140) uniform TShape
+{
+  layout(row_major) mat4 Move;
+}
+_Shape;
+
+////////////////////////////////////////////////////////////////////////////////【入出力】
+
+in struct TVert
+{
+  vec4 Pos;
+  vec4 Col;
+}
+_Vert;
+
+out TSendVF
+{
+  vec4 Col;
+}
+_Result;
+
+//------------------------------------------------------------------------------
+
 void main()
 {
-  gl_Position   = gl_ModelViewProjectionMatrix * gl_Vertex;
-  gl_FrontColor = gl_Color;
+  gl_Position = _Camera.Proj *inverse( _Camera.Move )
+              * _Shape.Move
+              * _Vert.Pos;
+  _Result.Col = _Vert.Col;
 }
 
+//##############################################################################
