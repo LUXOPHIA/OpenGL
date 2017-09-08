2e6df33cb7cf453ca4691cab13ae855064460cc7
luxidea <contact@luxidea.net>
2017-09-09 03:00:59 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX.GPU.OpenGL/' changes from 1c9659b6..ac5344b3
    ac5344b3 ビットマップのアルファ値を１に固定。
    c7b95cd2 TSingleM4.Identify → Identity 訂正。
    b03ca6d7 アルファ値が未定となるミスを修正。
    8bbd0bc8 TGLProgra.GetProgramResourceLocation/Index メソッドによる代替。
    5b9edcdf Merge commit 'b9f5f08a2e99cff8d3e0c949e3c47a396030766a'
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX.GPU.OpenGL
    git-subtree-split: ac5344b38205b050703555fb669a5aed039e7c62

--- » ATOM/LUX.GPU.OpenGL.Atom.pas
+++ » ATOM/LUX.GPU.OpenGL.Atom.pas
@@ -92,7 +92,12 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        ///// イベント
        property OnLinked :TProc read GetOnLinked write SetOnLinked;
        ///// メソッド
+       function GetAttribLocation( const Name_:String ) :GLuint;
+       function GetUniformLocation( const Name_:String ) :GLuint;
+       function GetProgramResourceLocation( const Kind_:GLenum; const Name_:String ) :GLuint;
+       function GetUniformBlockIndex( const Name_:String ) :GLuint;
        function GetProgramResourceIndex( const Kind_:GLenum; const Name_:String ) :GLuint;
+       procedure BindFragDataLocation( const BinP_:GLuint; const Name_:String );
        procedure Attach( const Shader_:IGLShader );
        procedure Detach( const Shader_:IGLShader );
        procedure Link;
