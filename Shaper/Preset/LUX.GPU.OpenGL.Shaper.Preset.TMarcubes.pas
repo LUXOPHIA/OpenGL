5487d7f4e8b9b6c9fcdfc8f0440a98809fd1b048
luxidea <contact@luxidea.net>
2018-03-13 18:41:57 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX.GPU.OpenGL/' changes from 8617b08d..82414e8c
    82414e8c TColorMarcubes クラスを追加。
    aef7d060 GL_RGBA → GL_RGBA32F 定数へ訂正。
    4db1fd43 no message
    b005bd3b no message
    6559b276 TMarcubesMateryFacesRGB / MIR マテリアルを追加。
    acc73957 no message
    2205b785 シェーダに Threshold 変数を追加。
    99803435 no message
    c7e2fd90 no message
    d4d6227c no message
    6c67b131 シェーダのロードを、個別のマテリアル内で処理。
    0c296098 no message
    b8fe10f2 TBricS → TGriderS, _Grids → _Grider, _BricsN → _ItemBricsN 変数へ変更。
    7a0732f0 no message
    77dae19a ワイヤフレームシェーダを追加。
    a43f97c7 TGLRender.Color プロパティを追加。
    1699a3ef バグを修正。
    0010bca1 ワイヤーフレーム用のシェーダを追加。
    6388df87 no message
    f36be2e7 MargsX/Y/Z を１に。
    3e191128 初期の MargsX/Y/Z をゼロに。
    03732a5e no message
    51bbff2b no message
    cd0a557b TGLRender クラスを追加。
    dd2ceb6f TGLFramer/1/N クラスを追加。
    b186897e TGLChaner/1/N クラス化を追加。
    290cbeb0 LUX.GPU.OpenGL.Atom.Imager.D1/2/3.Preset, LUX.GPU.OpenGL.Matery.Imager.Preset  ユニットをフォルダへ整理。
    05d01989 VCL → _VCL フォルダへ改名。
    6ee44b55 LUX.GPU.OpenGL.Matery.Imager.Preset をフォルダへ整理。
    ba966d8b ユニットを階層フォルダで整理。
    f2d2a9be シェーダソースをリソースファイルとして追加。
    efcc9703 TGLShader.LoadFromResource メソッドを追加。
    6bff8e9c no message
    75270da0 _Size フィールドを追加。
    6adb1f81 _Voxels → _Grider クラスを TMarcubes クラスへ移動。
    dad461fe TGLImager2D_TAlphaColorF → TGLBricer2D_TAlphaColorF クラスへ改名。
    28a5e520 LUX.GPU.OpenGL.Atom.Imager.D1/2/3.Preset ユニットを追加。
    57f3824a Sample プロパティを廃止。
    272c0bd1 TGLImager1/2/3D_RGBA → TGLImager1/2/3D_TAlphaColorF クラスへ改名。
    1f098a55 TGLSample → TGLSamper クラスへ改名。
    dfc965a5 no message
    2cfa5fa7 LUX.GPU.OpenGL.Atom.Imager.D1/2/3.Preset ユニットを追加。
    40b8bfea LUX.GPU.OpenGL.Atom.Imager.D1/2/3 ユニットを追加。
    6fb1ac99 TMarcubes クラスを追加。
    61d9f425 Sample プロバティを廃止。
    1678a9b5 TGLImager1D_RGBA → TGLBricer1D_TAlphaColorF クラスへ改名。 TGLImager2D_RGBA → TGLBricer2D_TAlphaColorF クラスへ改名。
    ce7352f4 TGLBricer1D, TGLGrider1D, TGLBricer2D, TGLGrider2D クラスを追加。
    40009b40 TGLImager3D_RGBA → TGLBricer3D_AlphaColorF クラスへ改名。 TGLGrider3D_AlphaColorF, TGLBricer3D_Single, TGLGrider3D_Single クラスを追加。
    0a09e190 TGLBricer3D, TGLGrider3D クラスを追加。
    ac018d11 no message
    17905f85 TGLShaperZeroPoins / Lines / Trias クラスを追加。
    5d30bbdb no message
    7897dc99 Update README.md
    2df156e9 Update README.md
    1b456e47 ユニット別のREADMEを追加。
    20d702a2 Update LUX.GPU.OpenGL.pas.md
    ecf480e5 no message
    60e5f396 Update README.md
    ae943c84 TGLShaperPoin.CalcBouBox メソッドのバグを修正。
    0917e515 TGLViewer.OnCreateAnyWND / OnDestroAnyWND メソッドを追加。 TGLViewer.GetScreenScale メソッドを追加。
    50fbd152 TGLViewerForm.DoParentFormChanged メソッドを追加。 TGLViewerForm.CreateNew コンストラクタにおいて、一回でも Show を呼ばないとエラー。
    660ac5d1 TGLViewerForm クラスを追加。
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX.GPU.OpenGL
    git-subtree-split: 82414e8c1a74e3a1d412d0854e2b1a065b45d953

---
+++ Shaper/Preset/LUX.GPU.OpenGL.Shaper.Preset.TMarcubes.pas
@@ -0,0 +1,431 @@
+﻿unit LUX.GPU.OpenGL.Shaper.Preset.TMarcubes;
+
+interface //#################################################################### ■
+
+uses Winapi.OpenGL, Winapi.OpenGLext,
+     LUX, LUX.D2, LUX.D3, LUX.M4,
+     LUX.GPU.OpenGL,
+     LUX.GPU.OpenGL.Atom.Buffer.Unifor,
+     LUX.GPU.OpenGL.Atom.Imager.D2.Preset,
+     LUX.GPU.OpenGL.Atom.Imager.D3.Preset,
+     LUX.GPU.OpenGL.Matery,
+     LUX.GPU.OpenGL.Shaper;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     TMarcubes = class;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMarcubesMateryFacesRGB
+
+     IMarcubesMateryFacesRGB = interface( IGLMateryNorTexG )
+     ['{48DD7893-831C-4F39-ACA3-AFBC9F37DD73}']
+     {protected}
+     {public}
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TMarcubesMateryFacesRGB = class( TGLMateryNorTexG, IMarcubesMateryFacesRGB )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMarcubesMateryFacesMIR
+
+     IMarcubesMateryFacesMIR = interface( IGLMateryNorTexG )
+     ['{1FD800E2-12D5-4021-825F-368CEFFEFAA5}']
+     {protected}
+     {public}
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TMarcubesMateryFacesMIR = class( TGLMateryNorTexG, IMarcubesMateryFacesMIR )
+     private
+     protected
+       _Imager :TGLBricer2D_TAlphaColorF;
+     public
+       constructor Create;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Imager :TGLBricer2D_TAlphaColorF read _Imager;
+       ///// メソッド
+       procedure Use; override;
+       procedure Unuse; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMarcubesMateryCubes
+
+     IMarcubesMateryCubes = interface( IGLMateryNorTexG )
+     ['{9FD0E274-E7F3-4B90-85A5-21B0C52FC9CB}']
+     {protected}
+     {public}
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TMarcubesMateryCubes = class( TGLMateryNorTexG, IMarcubesMateryCubes )
+     private
+     protected
+     public
+       constructor Create;
+       destructor Destroy; override;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMarcubes
+
+     TMarcubes = class( TGLShaperZeroPoins )
+     private
+     protected
+       _MaterC      :IMarcubesMateryCubes;
+       _Grider      :TGLGrider3D_Single;
+       _Size        :TGLUnifor<TSingle3D>;
+       _Threshold   :TGLUnifor<Single>;
+       _LineS       :Single;
+       _IsShowCubes :Boolean;
+       ///// アクセス
+       function GetSizeX :Single;
+       procedure SetSizeX( const SizeX_:Single );
+       function GetSizeY :Single;
+       procedure SetSizeY( const SizeY_:Single );
+       function GetSizeZ :Single;
+       procedure SetSizeZ( const SizeZ_:Single );
+       function GetThreshold :Single;
+       procedure SetThreshold( const Threshold_:Single );
+     public
+       constructor Create; override;
+       destructor Destroy; override;
+       ///// プロパティ
+       property Grider      :TGLGrider3D_Single read   _Grider                          ;
+       property SizeX       :Single             read GetSizeX       write SetSizeX      ;
+       property SizeY       :Single             read GetSizeY       write SetSizeY      ;
+       property SizeZ       :Single             read GetSizeZ       write SetSizeZ      ;
+       property Threshold   :Single             read GetThreshold   write SetThreshold  ;
+       property LineS       :Single             read   _LineS       write   _LineS      ;
+       property IsShowCubes :Boolean            read   _IsShowCubes write   _IsShowCubes;
+       ///// メソッド
+       procedure BeginDraw; override;
+       procedure EndDraw; override;
+       procedure MakeModel;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMarcubesMateryFacesRGB
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TMarcubesMateryFacesRGB.Create;
+begin
+     inherited;
+
+     with _Engine do
+     begin
+          with Verters do
+          begin
+               Del( 0{BinP} );
+               Del( 1{BinP} );
+               Del( 2{BinP} );
+          end;
+
+          with Unifors do
+          begin
+               Add( 4{BinP}, 'TGriderS'{Name} );
+               Add( 5{BinP}, 'TThreshold'{Name} );
+          end;
+
+          with Imagers do
+          begin
+               Add( 0{BinP}, '_Grider'{Name} );
+          end;
+
+          ShaderV.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_Faces_V_glsl' );
+          ShaderG.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_Faces_G_glsl' );
+          ShaderF.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_FacesRGB_F_glsl' );
+     end;
+
+end;
+
+destructor TMarcubesMateryFacesRGB.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMarcubesMateryFacesMIR
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TMarcubesMateryFacesMIR.Create;
+begin
+     inherited;
+
+     with _Engine do
+     begin
+          with Verters do
+          begin
+               Del( 0{BinP} );
+               Del( 1{BinP} );
+               Del( 2{BinP} );
+          end;
+
+          with Unifors do
+          begin
+               Add( 4{BinP}, 'TGriderS'{Name} );
+               Add( 5{BinP}, 'TThreshold'{Name} );
+          end;
+
+          with Imagers do
+          begin
+               Add( 0{BinP}, '_Grider'{Name} );
+               Add( 1{BinP}, '_Imager'{Name} );
+          end;
+
+          ShaderV.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_Faces_V_glsl' );
+          ShaderG.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_Faces_G_glsl' );
+          ShaderF.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_FacesMIR_F_glsl' );
+     end;
+
+     _Imager := TGLBricer2D_TAlphaColorF.Create;
+end;
+
+destructor TMarcubesMateryFacesMIR.Destroy;
+begin
+     _Imager.DisposeOf;
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TMarcubesMateryFacesMIR.Use;
+begin
+     inherited;
+
+     _Imager.Use( 1 );
+end;
+
+procedure TMarcubesMateryFacesMIR.Unuse;
+begin
+     _Imager.Unuse( 1 );
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMarcubesMateryCubes
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TMarcubesMateryCubes.Create;
+begin
+     inherited;
+
+     with _Engine do
+     begin
+          with Verters do
+          begin
+               Del( 0{BinP} );
+               Del( 1{BinP} );
+               Del( 2{BinP} );
+          end;
+
+          with Unifors do
+          begin
+               Add( 4{BinP}, 'TGriderS'{Name} );
+               Add( 5{BinP}, 'TThreshold'{Name} );
+          end;
+
+          with Imagers do
+          begin
+               Add( 0{BinP}, '_Grider'{Name} );
+          end;
+
+          ShaderV.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_Cubes_V_glsl' );
+          ShaderG.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_Cubes_G_glsl' );
+          ShaderF.LoadFromResource( 'LUX_GPU_OpenGL_Shaper_Preset_TMarcubes_Cubes_F_glsl' );
+     end;
+end;
+
+destructor TMarcubesMateryCubes.Destroy;
+begin
+
+     inherited;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TMarcubes
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TMarcubes.GetSizeX :Single;
+begin
+     Result := _Size[ 0 ].X;
+end;
+
+procedure TMarcubes.SetSizeX( const SizeX_:Single );
+begin
+     _Size[ 0 ] := TSingle3D.Create( SizeX_, SizeY, SizeZ );
+end;
+
+function TMarcubes.GetSizeY :Single;
+begin
+     Result := _Size[ 0 ].Y;
+end;
+
+procedure TMarcubes.SetSizeY( const SizeY_:Single );
+begin
+     _Size[ 0 ] := TSingle3D.Create( SizeX, SizeY_, SizeZ );
+end;
+function TMarcubes.GetSizeZ :Single;
+begin
+     Result := _Size[ 0 ].Z;
+end;
+
+procedure TMarcubes.SetSizeZ( const SizeZ_:Single );
+begin
+     _Size[ 0 ] := TSingle3D.Create( SizeX, SizeY, SizeZ_ );
+end;
+
+//------------------------------------------------------------------------------
+
+function TMarcubes.GetThreshold :Single;
+begin
+     Result := _Threshold[ 0 ];
+end;
+
+procedure TMarcubes.SetThreshold( const Threshold_:Single );
+begin
+     _Threshold[ 0 ] := Threshold_;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TMarcubes.Create;
+begin
+     inherited;
+
+     _Grider    := TGLGrider3D_Single.Create;
+     _Size      := TGLUnifor<TSingle3D>.Create( GL_STATIC_DRAW );  _Size.Count := 1;
+     _Threshold := TGLUnifor<Single>.Create( GL_STATIC_DRAW );  _Threshold.Count := 1;
+
+     _Matery := TMarcubesMateryFacesRGB.Create;
+     _MaterC := TMarcubesMateryCubes.Create;
+
+     with Grider.Texels do
+     begin
+          MargsX := 1;
+          MargsY := 1;
+          MargsZ := 1;
+          BricsX := 100;
+          BricsY := 100;
+          BricsZ := 100;
+     end;
+
+     SizeX := 2;
+     SizeY := 2;
+     SizeZ := 2;
+
+     Threshold := 0.5;
+
+     _LineS := 1;
+
+     _IsShowCubes := False;
+end;
+
+destructor TMarcubes.Destroy;
+begin
+     _Grider   .DisposeOf;
+     _Size     .DisposeOf;
+     _Threshold.DisposeOf;
+
+     inherited;
+end;
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TMarcubes.BeginDraw;
+begin
+     inherited;
+
+     _Grider   .Use( 0 );
+     _Size     .Use( 4 );
+     _Threshold.Use( 5 );
+end;
+
+procedure TMarcubes.EndDraw;
+begin
+     if _IsShowCubes then
+     begin
+          _MaterC.Use;
+
+            glLineWidth( _LineS );
+
+            DrawMain;
+
+          _MaterC.Unuse;
+     end;
+
+     _Grider   .Unuse( 0 );
+     _Size     .Unuse( 4 );
+     _Threshold.Unuse( 5 );
+
+     inherited;
+end;
+
+procedure TMarcubes.MakeModel;
+begin
+     with _Grider do
+     begin
+          SendData;
+
+          with Texels do PoinsN := BricsX * BricsY * BricsZ;
+     end;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
