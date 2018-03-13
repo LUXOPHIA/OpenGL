7bf2befe675cf16563ea2b9ec5be1994658a6279
luxidea <contact@luxidea.net>
2018-03-13 18:37:44 +0900

    Squashed '_LIBRARY/LUXOPHIA/LUX/' changes from 62185d80..fee6b509
    fee6b509 LUX.Data.Lattice.T1.D1/2 ユニットを追加。
    6a3018b4 Merge commit '238b93dc7fc7e5fd43f72dc6b4e623a57c9a93f8'
    55f9a5ba IArray3D.Lines, LineSize プロパティを追加。
    2b5359f0 フォルダへ整理。
    6708fd07 Merge commit 'daab7de84ae618dc65121da453423cc90abfac77'
    1993011f TArray1D<_TItem_>.ElemsP0 プロパティを追加。 TArray2D<_TItem_>.ElemsP0 プロパティを追加。
    ada6833b TArray3D<_TItem_>.ElemsP0 プロパティを追加。
    2dba3d0c TSingleBricIterGridArray3D.FracGrad, Grad メソッドを追加。
    50bc21fa FracInterp → Interp, Interp → AbsoInterp メソッドへ改名。
    9f9d0fcb TInteger3D → TPoint3D, TSingle3D, TDouble3D 型変換を追加。
    e02f7871 LUX.Data.Octree.Atom ユニットを追加。
    470a021a TBricIterGridArray3D<_TItem_>.FracInterp を最近傍補間に変更。
    bab594ea TSingle2D ⇔ TPointF 型変換の定義を変更。
    b8e7f2e4 Merge commit '17151159d53a96f4c5048491e1fb1e38c31654c8'
    89f7010e TArray3D<_TItem_>.Elems, ElemP プロパティを追加。
    9b3a257f TSingleBricIterGridArray3D.Grad の微分値定義を修正。
    d00279b4 TBricIterGridArray3D<_TItem_>.InterpFrac → FracInterp メソッドへ改名。
    71fbb792 TBricIterGridArray3D<_TItem_>.Gi プロパティを追加。
    a755d524 TSingleGridArray3D.ForBrics, ForEdgesX/Y/Z メソッドを追加。
    b6cc1b8c TGridArray3D<_TItem_>.ForBrics, ForEdgesX/Y/Z メソッドを追加。
    71e0ebc2 _Array → _Paren
    baa3d342 TBricIterGridArray3D<_TItem_>.ForBrics, ForEdgesX/Y/Z メソッドを追加。
    f22c8709 TSingleBricIterGridArray3D.Grad メソッドを追加。
    46b9ed08 TSingle/Double3D → TPoint3D の型変換の座標軸反転を無効化。
    1b53c21f ItemByte, ElemsX/Y/Z,k ElemsN, ElemsByte プロパティを追加。
    eb5f2396 no message
    f9a262b7 _TotalX/Y → _ElemsX/Y へ改名。
    43f6b31d _Items → _Elems へ改名。
    55250dcb LUX.Data.Lattice.T3 ユニットを追加。
    ce669a37 TBricIterGridArray3D.Interp メソッドを追加。 TBricIterGridArray3D.PosX/Y/Z プロパティを追加。
    5abe5ab1 TArray3D ItemSize → ItemByte TotalN → ElemsN TotalSize → ElemsByte StepX → ByteStepX ElemStepX → CounStepX
    f1f483ec TBricIterGridArray3D クラスを追加。
    adaffacc PPLongint 型を追加。
    4d4ebd88 IncBeta 関数において、HypGeo21A 関数を利用。
    a8823823 連分数展開による IncBeta 関数を追加（実用には耐えない）。
    da3dac56 HypGeo21A( B_, C_, X_ ) = HypGeo21( 1, B_, C_, X_ ) 関数を追加。
    88e28791 FloatToStr 関数を NAN / -INF / +INF 値に対応。
    d963ca91 Merge commit 'de775b7dccc08eab78e63fe740f8afb580a3e8cc' into feature
    0b60a5f6 Merge commit '4d1e26a42095d4c3a01ecd007f46b2562fd213df' into feature
    96e1134d Merge commit '9052d1f1d0a125f597925fa2b9fd082639eda83f' into future_nakayama
    d3c402a8 Merge commit '988731f2f70fa6ce841b37d95c4ca2d75c1f6d83' into nakayama
    988731f2 Squashed '_LIBRARY/LUXOPHIA/LUX.Lattice/' changes from 0dc673aa..5c30c6f5
    6d00c8aa Merge commit 'a45acf59aaf1f7de2d6ee84bb278b7ea00b811af' into nakayama
    ed3d9830 Merge commit '81e820d33933894f82fc896a31a081c876c61b37' into nakayama
    d82d3d4a Merge commit 'b0184421c187f4dada566659ed7a7b8a03b66b0e' into nakayama
    5c22cd5d Merge commit '00b2b7f5d4637edcb8974bfcc95a6c76f27d6a5d' into nakayama
    e4a282e3 逆行列メソッド Inverse を追加。
    50720ea4 Merge commit '32ac0967ce001b68e03b649a80f9dbc6b5536576' into nakayama
    32ac0967 Squashed '_LIBRARY/LUXOPHIA/LUX.Lattice/' changes from 6eb976e..0dc673a
    76923420 Add '_LIBRARY/LUXOPHIA/LUX.Lattice/' from commit '6eb976eabd1ff0344623fc224a83f18096434f97'
    6eb976ea LUX.Matrix.L4 → LUX.M4 へ代替。
    3943dc0b バグ修正。
    24aaeae2 LUX.Lattice.T2.D1 ユニットを追加。
    e095a06f LUX.Map → LUX.Lattice へ改名。
    e45af67d README.md edited online with Bitbucket
    
    git-subtree-dir: _LIBRARY/LUXOPHIA/LUX
    git-subtree-split: fee6b50952692672ce9f90b134b8eb516c123f17

---
+++ Data/Lattice/T3/LUX.Data.Lattice.T3.D3.pas
@@ -0,0 +1,217 @@
+﻿unit LUX.Data.Lattice.T3.D3;
+
+interface //#################################################################### ■
+
+uses System.SysUtils, System.Classes,
+     LUX, LUX.D1, LUX.D3, LUX.Data.Lattice.T3;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【型】
+
+     TSingleBricArray3D         = class;
+     TSingleGridArray3D         = class;
+     TSingleBricIterGridArray3D = class;
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleBricArray3D
+
+     ISingleBricArray3D = interface( IBricArray3D )
+     ['{07DD7FC2-8BD0-4D64-86E5-7FFDE4CF7CA3}']
+     {protected}
+     {public}
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TSingleBricArray3D = class( TBricArray3D<Single>, ISingleBricArray3D )
+     private
+     protected
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleGridArray3D
+
+     ISingleGridArray3D = interface( IGridArray3D )
+     ['{9E07E15D-0E29-4541-A755-53BABC4AC560}']
+     {protected}
+     {public}
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TSingleGridArray3D = class( TGridArray3D<Single>, ISingleGridArray3D )
+     private
+     protected
+     public
+       ///// メソッド
+       procedure ForBrics( const Proc_:TConstProc<TSingleBricIterGridArray3D> );
+       procedure ForEdgesX( const Proc_:TConstProc<TSingleBricIterGridArray3D> );
+       procedure ForEdgesY( const Proc_:TConstProc<TSingleBricIterGridArray3D> );
+       procedure ForEdgesZ( const Proc_:TConstProc<TSingleBricIterGridArray3D> );
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleBricIterGridArray3D
+
+     ISingleBricIterGridArray3D = interface( IBricIterGridArray3D<Single> )
+     ['{1FA11B62-B22A-4966-815F-48EF06FDBF3B}']
+     {protected}
+     {public}
+     end;
+
+     //-------------------------------------------------------------------------
+
+     TSingleBricIterGridArray3D = class( TBricIterGridArray3D<Single>, ISingleBricIterGridArray3D )
+     private
+     protected
+     public
+       ///// メソッド
+       function Interp( const Xd_,Yd_,Zd_:Single ) :Single; override;
+       function FracGrad( const Xd_,Yd_,Zd_:Single ) :TSingle3D; overload;
+       function Grad( const d_:TSingle3D ) :TSingle3D; overload;
+       function AbsoGrad( const X_,Y_,Z_:Single ) :TSingle3D;
+     end;
+
+//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【定数】
+
+//var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【変数】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+implementation //############################################################### ■
+
+uses System.Math;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【クラス】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleBricArray3D
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleGridArray3D
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+procedure TSingleGridArray3D.ForBrics( const Proc_:TConstProc<TSingleBricIterGridArray3D> );
+begin
+     inherited ForBrics( procedure( const B_:TBricIterGridArray3D<Single> )
+     begin
+          Proc_( B_ as TSingleBricIterGridArray3D );
+     end );
+end;
+
+procedure TSingleGridArray3D.ForEdgesX( const Proc_:TConstProc<TSingleBricIterGridArray3D> );
+begin
+     inherited ForEdgesX( procedure( const E_:TBricIterGridArray3D<Single> )
+     begin
+          Proc_( E_ as TSingleBricIterGridArray3D );
+     end );
+end;
+
+procedure TSingleGridArray3D.ForEdgesY( const Proc_:TConstProc<TSingleBricIterGridArray3D> );
+begin
+     inherited ForEdgesY( procedure( const E_:TBricIterGridArray3D<Single> )
+     begin
+          Proc_( E_ as TSingleBricIterGridArray3D );
+     end );
+end;
+
+procedure TSingleGridArray3D.ForEdgesZ( const Proc_:TConstProc<TSingleBricIterGridArray3D> );
+begin
+     inherited ForEdgesZ( procedure( const E_:TBricIterGridArray3D<Single> )
+     begin
+          Proc_( E_ as TSingleBricIterGridArray3D );
+     end );
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleBricIterGridArray3D
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+/////////////////////////////////////////////////////////////////////// メソッド
+
+function TSingleBricIterGridArray3D.Interp( const Xd_,Yd_,Zd_:Single ) :Single;
+var
+   Y00, Y01, Y10, Y11, Z0, Z1 :Single;
+begin
+     Y00 := ( Grids[ 1, 0, 0 ] - Grids[ 0, 0, 0 ] ) * Xd_ + Grids[ 0, 0, 0 ];
+     Y01 := ( Grids[ 1, 1, 0 ] - Grids[ 0, 1, 0 ] ) * Xd_ + Grids[ 0, 1, 0 ];
+     Y10 := ( Grids[ 1, 0, 1 ] - Grids[ 0, 0, 1 ] ) * Xd_ + Grids[ 0, 0, 1 ];
+     Y11 := ( Grids[ 1, 1, 1 ] - Grids[ 0, 1, 1 ] ) * Xd_ + Grids[ 0, 1, 1 ];
+
+     Z0 := ( Y01 - Y00 ) * Yd_ + Y00;
+     Z1 := ( Y11 - Y10 ) * Yd_ + Y10;
+
+     Result := ( Z1 - Z0 ) * Zd_ + Z0;
+end;
+
+//------------------------------------------------------------------------------
+
+function TSingleBricIterGridArray3D.FracGrad( const Xd_,Yd_,Zd_:Single ) :TSingle3D;
+//······································
+     function GetInterp( const X,Y,Z:Shortint ) :Single;
+     var
+        Y00, Y01, Y10, Y11, Z0, Z1 :Single;
+     begin
+          Y00 := ( Grids[ X+1, Y+0, Z+0 ] - Grids[ X+0, Y+0, Z+0 ] ) * Xd_ + Grids[ X+0, Y+0, Z+0 ];
+          Y01 := ( Grids[ X+1, Y+1, Z+0 ] - Grids[ X+0, Y+1, Z+0 ] ) * Xd_ + Grids[ X+0, Y+1, Z+0 ];
+          Y10 := ( Grids[ X+1, Y+0, Z+1 ] - Grids[ X+0, Y+0, Z+1 ] ) * Xd_ + Grids[ X+0, Y+0, Z+1 ];
+          Y11 := ( Grids[ X+1, Y+1, Z+1 ] - Grids[ X+0, Y+1, Z+1 ] ) * Xd_ + Grids[ X+0, Y+1, Z+1 ];
+
+          Z0 := ( Y01 - Y00 ) * Yd_ + Y00;
+          Z1 := ( Y11 - Y10 ) * Yd_ + Y10;
+
+          Result := ( Z1 - Z0 ) * Zd_ + Z0;
+     end;
+//······································
+begin
+     with Result do
+     begin
+          X := ( GetInterp( +1,  0,  0 ) - GetInterp( -1,  0,  0 ) ) / 2;
+          Y := ( GetInterp(  0, +1,  0 ) - GetInterp(  0, -1,  0 ) ) / 2;
+          Z := ( GetInterp(  0,  0, +1 ) - GetInterp(  0,  0, -1 ) ) / 2;
+     end;
+end;
+
+function TSingleBricIterGridArray3D.Grad( const d_:TSingle3D ) :TSingle3D;
+begin
+     Result := FracGrad( d_.X, d_.Y, d_.Z );
+end;
+
+function TSingleBricIterGridArray3D.AbsoGrad( const X_,Y_,Z_:Single ) :TSingle3D;
+var
+   Xd, Yd, Zd :Single;
+begin
+     PosZ := Floor( Z_ );  Zd := Z_ - PosZ;
+     PosY := Floor( Y_ );  Yd := Y_ - PosY;
+     PosX := Floor( X_ );  Xd := X_ - PosX;
+
+     Result := FracGrad( Xd, Yd, Zd );
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【ルーチン】
+
+//############################################################################## □
+
+initialization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 初期化
+
+finalization //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 最終化
+
+end. //######################################################################### ■
