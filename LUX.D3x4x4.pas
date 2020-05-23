9b645b583d5d1570bec75e8463da2c1f6279b73f
luxidea <contact@luxidea.net>
2020-05-24 06:07:55 +0900

    TSingle3DM4 → TSingle4x4x3D レコードへ改名。

--- LUX.D3x4x4.pas
+++ LUX.D3x4x4.pas
@@ -1,4 +1,4 @@
-﻿unit LUX.D3.M4;
+﻿unit LUX.D3x4x4;
 
 interface //#################################################################### ■
 
@@ -8,16 +8,16 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingle3DM4
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingle4x4x3D
 
-     TSingle3DM4 = record
+     TSingle4x4x3D = record
      private
        ///// アクセス
-       function GetM( const Y_,X_:Integer ) :TSingle3D; inline;
-       procedure SetM( const Y_,X_:Integer; const M_:TSingle3D ); inline;
+       function Gets( const Y_,X_:Integer ) :TSingle3D; inline;
+       procedure Sets( const Y_,X_:Integer; const M_:TSingle3D ); inline;
      public
        ///// プロパティ
-       property M[ const Y_,X_:Integer ] :TSingle3D read GetM write SetM; default;
+       property _s[ const Y_,X_:Integer ] :TSingle3D read Gets write Sets; default;
      case Integer of
       0:( _ :array [ 1..4, 1..4 ] of TSingle3D; );
       1:( _11, _12, _13, _14,
@@ -26,16 +26,16 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
           _41, _42, _43, _44 :TSingle3D;        );
      end;
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TDouble3DM4
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TDouble4x4x3D
 
-     TDouble3DM4 = record
+     TDouble4x4x3D = record
      private
        ///// アクセス
-       function GetM( const Y_,X_:Integer ) :TDouble3D; inline;
-       procedure SetM( const Y_,X_:Integer; const M_:TDouble3D ); inline;
+       function Gets( const Y_,X_:Integer ) :TDouble3D; inline;
+       procedure Sets( const Y_,X_:Integer; const M_:TDouble3D ); inline;
      public
        ///// プロパティ
-       property M[ const Y_,X_:Integer ] :TDouble3D read GetM write SetM; default;
+       property _s[ const Y_,X_:Integer ] :TDouble3D read Gets write Sets; default;
      case Integer of
       0:( _ :array [ 1..4, 1..4 ] of TDouble3D; );
       1:( _11, _12, _13, _14,
@@ -44,19 +44,19 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
           _41, _42, _43, _44 :TDouble3D;        );
      end;
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TdSingle3DM4
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TdSingle4x4x3D
 
-     TdSingle3DM4 = record
+     TdSingle4x4x3D = record
      private
        ///// アクセス
-       function GetM( const Y_,X_:Integer ) :TdSingle3D; inline;
-       procedure SetM( const Y_,X_:Integer; const M_:TdSingle3D ); inline;
+       function Gets( const Y_,X_:Integer ) :TdSingle3D; inline;
+       procedure Sets( const Y_,X_:Integer; const M_:TdSingle3D ); inline;
      public
        ///// プロパティ
-       property M[ const Y_,X_:Integer ] :TdSingle3D read GetM write SetM; default;
+       property _s[ const Y_,X_:Integer ] :TdSingle3D read Gets write Sets; default;
        ///// 型変換
-       class operator Implicit( const M_:TSingle3DM4 ) :TdSingle3DM4; inline;
-       class operator Explicit( const M_:TdSingle3DM4 ) :TSingle3DM4; inline;
+       class operator Implicit( const M_:TSingle4x4x3D ) :TdSingle4x4x3D; inline;
+       class operator Explicit( const M_:TdSingle4x4x3D ) :TSingle4x4x3D; inline;
      case Integer of
       0:( _ :array [ 1..4, 1..4 ] of TdSingle3D; );
       1:( _11, _12, _13, _14,
@@ -65,19 +65,19 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
           _41, _42, _43, _44 :TdSingle3D;        );
      end;
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TdDouble3DM4
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TdDouble4x4x3D
 
-     TdDouble3DM4 = record
+     TdDouble4x4x3D = record
      private
        ///// アクセス
-       function GetM( const Y_,X_:Integer ) :TdDouble3D; inline;
-       procedure SetM( const Y_,X_:Integer; const M_:TdDouble3D ); inline;
+       function Gets( const Y_,X_:Integer ) :TdDouble3D; inline;
+       procedure Sets( const Y_,X_:Integer; const M_:TdDouble3D ); inline;
      public
        ///// プロパティ
-       property M[ const Y_,X_:Integer ] :TdDouble3D read GetM write SetM; default;
+       property _s[ const Y_,X_:Integer ] :TdDouble3D read Gets write Sets; default;
        ///// 型変換
-       class operator Implicit( const M_:TDouble3DM4 ) :TdDouble3DM4; inline;
-       class operator Explicit( const M_:TdDouble3DM4 ) :TDouble3DM4; inline;
+       class operator Implicit( const M_:TDouble4x4x3D ) :TdDouble4x4x3D; inline;
+       class operator Explicit( const M_:TdDouble4x4x3D ) :TDouble4x4x3D; inline;
      case Integer of
       0:( _ :array [ 1..4, 1..4 ] of TdDouble3D; );
       1:( _11, _12, _13, _14,
@@ -98,94 +98,52 @@ implementation //###############################################################
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【レコード】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingle3DM4
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingle4x4x3D
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
 /////////////////////////////////////////////////////////////////////// アクセス
 
-function TSingle3DM4.GetM( const Y_,X_:Integer ) :TSingle3D;
+function TSingle4x4x3D.Gets( const Y_,X_:Integer ) :TSingle3D;
 begin
      Result := _[ Y_, X_ ];
 end;
 
-procedure TSingle3DM4.SetM( const Y_,X_:Integer; const M_:TSingle3D );
+procedure TSingle4x4x3D.Sets( const Y_,X_:Integer; const M_:TSingle3D );
 begin
      _[ Y_, X_ ] := M_;
 end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TDouble3DM4
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TDouble4x4x3D
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
 /////////////////////////////////////////////////////////////////////// アクセス
 
-function TDouble3DM4.GetM( const Y_,X_:Integer ) :TDouble3D;
+function TDouble4x4x3D.Gets( const Y_,X_:Integer ) :TDouble3D;
 begin
      Result := _[ Y_, X_ ];
 end;
 
-procedure TDouble3DM4.SetM( const Y_,X_:Integer; const M_:TDouble3D );
+procedure TDouble4x4x3D.Sets( const Y_,X_:Integer; const M_:TDouble3D );
 begin
      _[ Y_, X_ ] := M_;
 end;
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TdSingle3DM4
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TdSingle4x4x3D
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
 /////////////////////////////////////////////////////////////////////// アクセス
 
-function TdSingle3DM4.GetM( const Y_,X_:Integer ) :TdSingle3D;
+function TdSingle4x4x3D.Gets( const Y_,X_:Integer ) :TdSingle3D;
 begin
      Result := _[ Y_, X_ ];
 end;
 
-procedure TdSingle3DM4.SetM( const Y_,X_:Integer; const M_:TdSingle3D );
-begin
-     _[ Y_, X_ ] := M_;
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-///////////////////////////////////////////////////////////////////////// 型変換
-
-class operator TdSingle3DM4.Implicit( const M_:TSingle3DM4 ) :TdSingle3DM4;
-begin
-     with Result  do
-     begin
-          _11 := M_._11;  _12 := M_._12;  _13 := M_._13;  _14 := M_._14;
-          _21 := M_._21;  _22 := M_._22;  _23 := M_._23;  _24 := M_._24;
-          _31 := M_._31;  _32 := M_._32;  _33 := M_._33;  _34 := M_._34;
-          _41 := M_._41;  _42 := M_._42;  _43 := M_._43;  _44 := M_._44;
-     end;
-end;
-
-class operator TdSingle3DM4.Explicit( const M_:TdSingle3DM4 ) :TSingle3DM4;
-begin
-     with Result  do
-     begin
-          _11 := M_._11;  _12 := M_._12;  _13 := M_._13;  _14 := M_._14;
-          _21 := M_._21;  _22 := M_._22;  _23 := M_._23;  _24 := M_._24;
-          _31 := M_._31;  _32 := M_._32;  _33 := M_._33;  _34 := M_._34;
-          _41 := M_._41;  _42 := M_._42;  _43 := M_._43;  _44 := M_._44;
-     end;
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TdDouble3DM4
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-/////////////////////////////////////////////////////////////////////// アクセス
-
-function TdDouble3DM4.GetM( const Y_,X_:Integer ) :TdDouble3D;
-begin
-     Result := _[ Y_, X_ ];
-end;
-
-procedure TdDouble3DM4.SetM( const Y_,X_:Integer; const M_:TdDouble3D );
+procedure TdSingle4x4x3D.Sets( const Y_,X_:Integer; const M_:TdSingle3D );
 begin
      _[ Y_, X_ ] := M_;
 end;
@@ -194,7 +152,7 @@ end;
 
 ///////////////////////////////////////////////////////////////////////// 型変換
 
-class operator TdDouble3DM4.Implicit( const M_:TDouble3DM4 ) :TdDouble3DM4;
+class operator TdSingle4x4x3D.Implicit( const M_:TSingle4x4x3D ) :TdSingle4x4x3D;
 begin
      with Result  do
      begin
@@ -205,7 +163,49 @@ begin
      end;
 end;
 
-class operator TdDouble3DM4.Explicit( const M_:TdDouble3DM4 ) :TDouble3DM4;
+class operator TdSingle4x4x3D.Explicit( const M_:TdSingle4x4x3D ) :TSingle4x4x3D;
+begin
+     with Result  do
+     begin
+          _11 := M_._11;  _12 := M_._12;  _13 := M_._13;  _14 := M_._14;
+          _21 := M_._21;  _22 := M_._22;  _23 := M_._23;  _24 := M_._24;
+          _31 := M_._31;  _32 := M_._32;  _33 := M_._33;  _34 := M_._34;
+          _41 := M_._41;  _42 := M_._42;  _43 := M_._43;  _44 := M_._44;
+     end;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TdDouble4x4x3D
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
+
+/////////////////////////////////////////////////////////////////////// アクセス
+
+function TdDouble4x4x3D.Gets( const Y_,X_:Integer ) :TdDouble3D;
+begin
+     Result := _[ Y_, X_ ];
+end;
+
+procedure TdDouble4x4x3D.Sets( const Y_,X_:Integer; const M_:TdDouble3D );
+begin
+     _[ Y_, X_ ] := M_;
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+///////////////////////////////////////////////////////////////////////// 型変換
+
+class operator TdDouble4x4x3D.Implicit( const M_:TDouble4x4x3D ) :TdDouble4x4x3D;
+begin
+     with Result  do
+     begin
+          _11 := M_._11;  _12 := M_._12;  _13 := M_._13;  _14 := M_._14;
+          _21 := M_._21;  _22 := M_._22;  _23 := M_._23;  _24 := M_._24;
+          _31 := M_._31;  _32 := M_._32;  _33 := M_._33;  _34 := M_._34;
+          _41 := M_._41;  _42 := M_._42;  _43 := M_._43;  _44 := M_._44;
+     end;
+end;
+
+class operator TdDouble4x4x3D.Explicit( const M_:TdDouble4x4x3D ) :TDouble4x4x3D;
 begin
      with Result  do
      begin
